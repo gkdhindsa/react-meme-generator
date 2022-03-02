5846616684008be1ef1abe6270618b016d893cb0
Gurnehmat K. Dhindsa <gurnehmat.dhindsa@gmail.com>
2022-03-02 23:28:30 +0530

    Header

---
+++ react-meme-generator/src/components/Header.js
@@ -0,0 +1,15 @@
+
+function Header() {
+    return ( 
+        <header>
+            <span className="brand">
+                <img src="./images/troll_face.png" alt="troll face" className="troll" />
+                <h2>Meme generator</h2>
+            </span>
+            
+            <h4>Developed by GKD</h4>
+        </header>
+     );
+}
+
+export default Header;
