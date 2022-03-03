6dc6ae31bd721c8bc5b0c69ddfe0a2bd3903df77
Gurnehmat K. Dhindsa <gurnehmat.dhindsa@gmail.com>
2022-03-03 21:30:25 +0530

    Final commit

--- react-meme-generator/src/components/Meme.js
+++ react-meme-generator/src/components/Meme.js
@@ -1,12 +1,24 @@
+import React, {useState} from "react";
+import memesData from "../memesData";
 function Meme() {
+    const [url, setUrl]=useState("")
+    function getMemeImage(e)
+    {
+        e.preventDefault()
+        let items=memesData.data.memes
+        var item = items[Math.floor(Math.random()*items.length)];
+        setUrl(item.url)
+    }
     return (  
         <div className="container">
             <form className="meme-form">
                 <div className="inputs">
-                    <input type="text" name="text-1" placeholder="type here..."/>
-                    <input type="text" name="text-2" placeholder="type here..."/>
+                    <input type="text" name="text-1" placeholder="Top text"/>
+                    <span className="space"></span>
+                    <input type="text" name="text-2" placeholder="Bottom text"/>
                 </div>
-                <button type="submit">Get a new meme image 📷</button>
+                <button  onClick={getMemeImage}>Get a new meme image 📷</button>
+                <img src={url} alt="" className="meme" />
             </form>
         </div>
        
